6a3ee79cdc6cef2c348db4938e5fa43d5121f2fd
Paulo Victor Frazão <128742259+Pvictorfrazao@users.noreply.github.com>
2024-12-05 23:26:41 -0300

    Add files via upload

---
+++ Sort Int/src/BenchmarkData.java
@@ -0,0 +1,14 @@
+public class BenchmarkData {
+    public String algoritmo;
+    public int tamanhoAmostra;
+    public String cenario;
+    public double tempoExecucao;
+
+    // Construtor
+    public BenchmarkData(String algoritmo, int tamanhoAmostra, String cenario, double tempoExecucao) {
+        this.algoritmo = algoritmo;
+        this.tamanhoAmostra = tamanhoAmostra;
+        this.cenario = cenario;
+        this.tempoExecucao = tempoExecucao;
+    }
+}
